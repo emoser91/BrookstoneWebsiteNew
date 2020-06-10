db3e73fd998acd5e1df484475e85464fed7ec64d
Eric Moser <emoser91@gmail.com>
2020-06-09 19:57:00 -0500

    Redesign of site

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "main@ericmoser.com";
+
+    $headers = "From: ".$mailFrom;
+    
+    //Adding the rest of these helped make the email not go to spam in gmail
+    $headers = "Reply-To: ".$mailFrom;
+    $headers = "Return-Path: ".$mailFrom;
+    $headers = "CC: ".$mailFrom;
+    $headers = "BCC: ".$mailFrom;
+
+    $txt = "You have received a Quote Request e-mail from  ".$name."/n/n".$message;
+
+    if ( mail($mailTo,$subject,$txt,$headers) ) {
+        echo "The email has been sent!";
+    } else {
+        echo "The email has failed!";
+    }
+
+    // mail($mailTo, $subject, $txt, $headers);
+    // mail($mailFrom, $subject, $txt, $headers);
+    // header("Location: requestaquote.html?mailsend");
+
+}
+
+?> 
